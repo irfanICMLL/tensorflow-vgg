f8064f025e2100af544ae69233adc418714021fe
irfanICMLL <irfan111@163.com>
2017-03-27 11:31:08 +0800

    Update vgg16.py

--- vgg16.py
+++ vgg16.py
@@ -33,15 +33,11 @@ class Vgg16:
 
         # Convert RGB to BGR
         red, green, blue = tf.split(axis=3, num_or_size_splits=3, value=rgb_scaled)
-        assert red.get_shape().as_list()[1:] == [224, 224, 1]
-        assert green.get_shape().as_list()[1:] == [224, 224, 1]
-        assert blue.get_shape().as_list()[1:] == [224, 224, 1]
         bgr = tf.concat(axis=3, values=[
             blue - VGG_MEAN[0],
             green - VGG_MEAN[1],
             red - VGG_MEAN[2],
-        ])
-        assert bgr.get_shape().as_list()[1:] == [224, 224, 3]
+       
 
         self.conv1_1 = self.conv_layer(bgr, "conv1_1")
         self.conv1_2 = self.conv_layer(self.conv1_1, "conv1_2")
@@ -66,16 +62,7 @@ class Vgg16:
         self.conv5_3 = self.conv_layer(self.conv5_2, "conv5_3")
         self.pool5 = self.max_pool(self.conv5_3, 'pool5')
 
-        self.fc6 = self.fc_layer(self.pool5, "fc6")
-        assert self.fc6.get_shape().as_list()[1:] == [4096]
-        self.relu6 = tf.nn.relu(self.fc6)
-
-        self.fc7 = self.fc_layer(self.relu6, "fc7")
-        self.relu7 = tf.nn.relu(self.fc7)
-
-        self.fc8 = self.fc_layer(self.relu7, "fc8")
-
-        self.prob = tf.nn.softmax(self.fc8, name="prob")
+        
 
         self.data_dict = None
         print(("build model finished: %ds" % (time.time() - start_time)))
@@ -93,27 +80,12 @@ class Vgg16:
             conv = tf.nn.conv2d(bottom, filt, [1, 1, 1, 1], padding='SAME')
 
             conv_biases = self.get_bias(name)
-            bias = tf.nn.bias_add(conv, conv_biases)
+            bias = tf.nn.bias_add(conv, conv_biases
 
             relu = tf.nn.relu(bias)
             return relu
 
-    def fc_layer(self, bottom, name):
-        with tf.variable_scope(name):
-            shape = bottom.get_shape().as_list()
-            dim = 1
-            for d in shape[1:]:
-                dim *= d
-            x = tf.reshape(bottom, [-1, dim])
-
-            weights = self.get_fc_weight(name)
-            biases = self.get_bias(name)
-
-            # Fully connected layer. Note that the '+' operation automatically
-            # broadcasts the biases.
-            fc = tf.nn.bias_add(tf.matmul(x, weights), biases)
-
-            return fc
+    
 
     def get_conv_filter(self, name):
         return tf.constant(self.data_dict[name][0], name="filter")
@@ -121,5 +93,4 @@ class Vgg16:
     def get_bias(self, name):
         return tf.constant(self.data_dict[name][1], name="biases")
 
-    def get_fc_weight(self, name):
-        return tf.constant(self.data_dict[name][0], name="weights")
+    
